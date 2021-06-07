71a125d84e7cb1f25bae06178528dc76d2a932fa
Elton Santos <elton.santos07@outlook.com>
2021-06-07 02:37:07 -0300

    Commit inicial

---
+++ src/main/java/br/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,61 @@
+package br.org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String titulo;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private boolean vendaSemReceita;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public boolean isVendaSemReceita() {
+		return vendaSemReceita;
+	}
+
+	public void setVendaSemReceita(boolean vendaSemReceita) {
+		this.vendaSemReceita = vendaSemReceita;
+	}
+	
+	
+
+}
